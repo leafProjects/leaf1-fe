b6f8a2c9f1dc8a3dd9847fdb3ddd834e84616fb5
Pilone Matteo <pilone.matteo@gmail.com>
2019-07-05 15:06:19 +0200

    Rivisitazione Form FE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
@@ -18,7 +18,8 @@ import { UserService } from './user-service.service';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,29 +1,36 @@
 
 
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../user-service.service';
 import { User } from '../user';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css']
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnInit {
 
-  user: User;
+  form: FormGroup;
 
-  constructor(private route: ActivatedRoute, private router: Router, private userService: UserService) {
-    this.user = new User();
+  constructor(
+    private route: ActivatedRoute, private router: Router, private userService: UserService, private builder: FormBuilder) {
   }
-
-  // il metodo onSubmit() è usato per salvare l'oggetto User fornito
-  onSubmit() {
-    this.userService.signupUser(this.user).subscribe(result => this.gotoUserList());
+// il metodo onSubmit() è usato per salvare l'oggetto User fornito
+  singup(value) {
+    this.userService.signupUser(value).subscribe(result => this.gotoUserList());
   }
 
   gotoUserList() {
     this.router.navigate(['/users']);
+  }
+
+  ngOnInit(): void {
+    this.form = this.builder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 }
